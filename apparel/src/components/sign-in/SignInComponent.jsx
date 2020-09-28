24e80741c74c1d2eb85f1c9cb8999b185ac629cd
raj <raj@localhost.localdomain>
2020-09-28 21:55:56 +0530

    Change localhost reference to server address

--- apparel/src/components/sign-in/SignInComponent.jsx
+++ apparel/src/components/sign-in/SignInComponent.jsx
@@ -50,7 +50,7 @@ class SignInComponent extends React.Component{
 
     handleGoogleSignIn = e => {
 
-        window.open('http://localhost:5000/sign-in-with-google', "_self")
+        window.open('http://apparels-web:5000/sign-in-with-google', "_self")
     }
 
     render(){
