b492d66430ef1c0c928d0fce512f07bb343f5c58
raj <raj@localhost.localdomain>
2020-10-06 01:29:22 +0530

    Add redux-persist to store cart info

--- apparel/src/redux/root-reducer.js
+++ apparel/src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/Reducer'
 import cartReducer from './cart/Reducer'
 import inventoryReducer from './inventory/Reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
     inventory: inventoryReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
